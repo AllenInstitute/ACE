bc1a268cd5bee8c373bab4f57ead12670b3e824f
jeremymiller <jeremym@alleninstitute.org>
2025-05-21 15:21:50 -0700

    Fix breaking color error

--- R/auto_annotate.R
+++ R/auto_annotate.R
@@ -20,7 +20,8 @@ refactorize_annotations <- function(anno, metadata){
     
     if((sum(colnames(metadata)=="cell_type")==1)&(length(intersecting_cell_types)>0)){
       colors <- anno[,paste0(cn,"_color")][match(new_levels,anno[,paste0(cn,"_label")])]
-      colors[match(intersecting_cell_types,new_levels)] <- metadata$color[match(intersecting_cell_types,metadata$cell_type)]
+      if("color" %in% colnames(metadata))
+        colors[match(intersecting_cell_types,new_levels)] <- metadata$color[match(intersecting_cell_types,metadata$cell_type)]
       anno[,paste0(cn,"_color")] <- colors[match(anno[,paste0(cn,"_label")],new_levels)]
       new_levels <- c(intersecting_cell_types,setdiff(as.character(anno[,paste0(cn,"_label")]),metadata$cell_type))
       anno[,paste0(cn,"_id")]  <- as.numeric(factor(anno[,paste0(cn,"_label")],levels=new_levels))
